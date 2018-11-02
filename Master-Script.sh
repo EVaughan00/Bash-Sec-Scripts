db2b7f541d0cb2170c3e25df618650ece472cd04
EVaughan00 <35357260+EVaughan00@users.noreply.github.com>
2018-11-02 16:53:41 -0600

    Update Master-Script.sh

--- Master-Script.sh
+++ Master-Script.sh
@@ -110,20 +110,20 @@ echo
 ss -ln | grep LISTEN | grep -v 127.0
 echo 
 echo 
-echo "What services should I take note of?"
-echo
-read S
-for i in $S
-do
-ss -ln | grep $i > ./Notes.txt
-done
-echo
-echo "Which ports should I block?"
-read a
-for port in $a
-do
-/sbin/iptables -A INPUT -p tcp --destination-port $port -j DROP
-done
+#echo "What services should I take note of?"
+#echo
+#read S
+#for i in $S
+#do
+#ss -ln | grep $i > ./Notes.txt
+#done
+#echo
+#echo "Which ports should I block?"
+#read a
+#for port in $a
+#do
+#/sbin/iptables -A INPUT -p tcp --destination-port $port -j DROP
+#done
 
 a=("bin" "boot" "dev" "etc" "home" "lib" "lib64" "run" "sbin" "usr" "var" "media" "mnt" "opt" "srv")
 
