bf63f51ed8ad6bda57d656b43b707ea8cc802742
EVaughan00 <35357260+EVaughan00@users.noreply.github.com>
2018-11-02 15:59:48 -0600

    Update Master-Script.sh

--- Master-Script.sh
+++ Master-Script.sh
@@ -22,6 +22,23 @@ do
 ls -RA * | grep $i > /home/malware.txt
 find . *"$i" > /home/malwareMore.txt
 done
+echo "Installing clamav"
+echo
+apt-get install clamav
+systemctl stop clamav-freshclam
+freshclam
+echo
+systemctl start clamav-freshclam
+echo "Where would you like clam to scan?"
+read res
+clamscan -i -r --max-scansize=4000M --max-filesize=4000M ~/$res
+echo 
+echo
+echo "installing rkhunter"
+
+apt-get install rkhunter
+rkhunter --update
+rkhunter --propupd
 
 ufw enable 
 
